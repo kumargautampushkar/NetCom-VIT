f2ab09ad66e9893324256f3ca959d31823650918
Pushkar Kumar Gautam <kumargautampushkar@gmail.com>
2023-06-05 00:08:25 +0530

    Final commit in crc code

--- Error_detection_correction/crc.cpp
+++ Error_detection_correction/crc.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include "verification.hh"
+#include "error_module.hh"
 using namespace std;
 
 
@@ -76,6 +77,19 @@ string crc_generate(string div,string fx){
     return binary_modulo(div,fx);
 }
 
+bool crc_check(string data, string fx){
+    string crc_dat=crc_generate(data,fx);
+    string zero="";
+    for(int i=0;i<fx.size()-1;i++){
+        zero.push_back('0');
+    }
+
+    if(crc_dat==zero){
+        return true;
+    }
+    
+    return false;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -88,6 +102,7 @@ int main(int argc, char const *argv[])
         cout<<"Terminating the program"<<endl;
         return 0;
     }
+    cout<<endl;
     
     cout<<"Write the encoding function"<<endl;
     string fx;
@@ -95,9 +110,29 @@ int main(int argc, char const *argv[])
     if(!(is_crc_function_valid(data,fx))){
         return 0;
     }
+    cout<<endl;
 
     string gen_crc=crc_generate(data,fx);
-    cout<<gen_crc<<endl;
+    cout<<"The generated crc for the input is "<<gen_crc<<endl;
+    cout<<endl;
 
+    string sender_data=data+gen_crc;
+    cout<<"The data to be send >> "<<sender_data<<endl;
+    cout<<endl;
+
+    string reciever_data=sender_data;
+    singlebiterror_interactive(reciever_data);
+    cout<<endl;
+    
+    cout<<"The reciever end data is "<<reciever_data<<endl;
+    cout<<endl;
+    
+    bool isNotError=crc_check(reciever_data,fx);
+    if(isNotError){
+        cout<<"No error detected"<<endl;
+    }
+    else{
+        cout<<"Error detected in recieved data"<<endl;
+    }
     return 0;
 }
