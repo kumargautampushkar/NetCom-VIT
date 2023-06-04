0c5b5ed18e4b926bcf46ab6250d9d850a9f64620
Pushkar Kumar Gautam <kumargautampushkar@gmail.com>
2023-06-04 10:54:52 +0530

    The binary modulo function is properly working

--- Error_detection_correction/crc.cpp
+++ Error_detection_correction/crc.cpp
@@ -35,26 +35,42 @@ string popfront(string str){
     return temp;
 }
 
-string xor_modified(string const &s1, string const &s2)
-{
-    if(s1.size()!= s2.size())
-    {
-        cout<<"Error: Both the strings are not equal\nReturning empty string\nXOR operation unsuccessful";
-        return "";
+string binary_modulo(string div,string fx){
+    int p= div.size();
+    int q = fx.size();
+    string zero="";
+    for(int i=0;i<q;i++){
+        zero.push_back('0');
     }
-    string ans="";
-    for(int i=1;i<s1.size();i++){
-        if(s1[i] == s2[i]){
-            ans.push_back('0');
+
+    string rem= div.substr(0,q);
+    rem=bitwise_xor(rem,fx);
+
+    for(int i=0;i<p-q;i++){
+        //cout<<div[i+4];
+        rem=popfront(rem);
+        rem.push_back(div[i+4]);
+        if(rem[0]=='1'){
+            rem=bitwise_xor(rem,fx);
         }
         else{
-            ans.push_back('1');
+            rem=bitwise_xor(rem,zero);
         }
     }
-    return ans;
+    
+    return popfront(rem);
 }
 
 
+int main(int argc, char const *argv[])
+{
+    string s1,s2;
+    cin>>s1>>s2;
+    cout<<binary_modulo(s1,s2)<<endl;
+
+    return 0;
+}
+/*
 string crc(string div, string fx){
     string zero=fx;
     for(int i=0;i<fx.size();i++){
@@ -86,15 +102,38 @@ string crc(string div, string fx){
 
 }
 
+string binary_div(string div, string fx){
+    string zero=fx;
+    for(int i=0;i<fx.size();i++){
+        zero[i]='0';
+    }
 
-int main(int argc, char const *argv[])
-{
-    string s1,s2;
-    cin>>s1>>s2;
+    // for(int i=0;i<fx.size()-1;i++){
+    //     div.push_back('0');
+    // }
 
-    string x = crc(s1,s2);
-    cout<<x<<endl;
-    string s3= s1+x;
-    cout<<crc(s3,s2)<<endl;
-    return 0;
+    string ans=div.substr(0,fx.size());
+    if(ans[0]=='1'){
+        ans=bitwise_xor(ans,fx);
+    }
+    else{
+        ans=bitwise_xor(ans,zero);
+    }
+    for(int i=0;i<div.size()-1;i++){
+        if(ans[0]=='1'){
+            ans=popfront(ans);
+            ans.push_back(div[i+4]);
+            ans=bitwise_xor(ans,fx);
+        }else{
+            ans=popfront(ans);
+            ans.push_back(div[i+4]);
+            ans=bitwise_xor(ans,zero);
+        }
+    }
+    ans=popfront(ans);
+    return ans;
+
 }
+
+*/
+
