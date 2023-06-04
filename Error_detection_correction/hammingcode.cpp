2f4f5cf78a79695d8e549cba2ce42f87821b5489
Pushkar Kumar Gautam <kumargautampushkar@gmail.com>
2023-06-05 00:48:01 +0530

    removed buggy part

--- Error_detection_correction/hammingcode.cpp
+++ Error_detection_correction/hammingcode.cpp
@@ -1,27 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string hamming_code(string data){
-    int n=data.size();
-    int r=0;
-    while(pow(2,r)<n+r+1){
-        r++;
-    }
-    int k=n+r;
-    string ans="";
-    int j=0;
-    for(int i=1;i<=k;i++){
-        if(i==pow(2,j)){
-            ans.push_back('0');
-            j++;
-        }
-        else{
-            ans.push_back(data[i-j-1]);
-        }
-    }
-    return ans;
-    
-}
+
 
 int main(int argc, char const *argv[])
 {
