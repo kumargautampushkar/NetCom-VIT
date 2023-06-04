d45c48515608570f2ecb9b2ebb41c8e56e809ce3
Pushkar Kumar Gautam <kumargautampushkar@gmail.com>
2023-06-04 12:26:14 +0530

    Added functionality of generating function verification (crc generating function)

--- Error_detection_correction/verification.hh
+++ Error_detection_correction/verification.hh
@@ -2,19 +2,52 @@
 using namespace std;
 
     
-    bool isBinaryStringValid(string const &s){
-        bool ans=true;
-        for(auto &it:s){
-            if(!(it=='0' || it=='1'))
-            {
-                ans=false;
-                break;
-            }
+bool isBinaryStringValid(string const &s){
+    bool ans=true;
+    for(auto &it:s){
+        if(!(it=='0' || it=='1'))
+        {
+            ans=false;
+            break;
         }
+    }
+    return ans;
+}
+
+bool isBlockSizeValid(string const & s, int bs){
+    if(s.size()%bs==0) return true;
+    else return false;
+}
+
+bool is_leeding_zero(string const &fx){
+    bool flag=true;
+    if(fx[0]=='0')
+    {
+        flag=false;
+    }
+    return flag;   
+}
+
+bool is_crc_function_valid(string const &data, string const& fx)
+{
+    bool ans=true;
+    
+    if(!(isBinaryStringValid(fx))){
+        ans=false;
+        cout<<"Error : The given string by user is not a binary string"<<endl;
         return ans;
     }
-
-    bool isBlockSizeValid(string const & s, int bs){
-        if(s.size()%bs==0) return true;
-        else return false;
+    if(is_leeding_zero(fx)){
+        ans=false;
+        cout<<"Error : Leading zero detected in generating function"<<endl;
+        return ans;
     }
+   
+    if(fx.size()<=1 && fx.size() >= data.size())
+    {
+        ans=false;
+        cout<<"Error : The size of generating function is not valid"<<endl;
+        return ans;
+    }
+    return ans;    
+}
